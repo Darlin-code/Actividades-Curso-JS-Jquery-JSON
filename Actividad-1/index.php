8be65512d019af4432169484c89086ada49b2041
Darlin Daniel Arias Méndez <darlin.informatica@gmail.com>
2024-01-14 09:14:40 -0400

    Created and updated files

--- Actividad-1/index.php
+++ Actividad-1/index.php
@@ -7,8 +7,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Darlin Daniel Arias Méndez</title>
+    <link rel="stylesheet" href="assets/css/styles-general.css">
 </head>
 <body>
-    
+    <div class="container">
+        <h1>Darlin Daniel Arias Méndez's Resume Registry</h1>
+        <a href="login.php">Please log in</a>
+        <p><b>Note: </b>Nothing</p>
+    </div>
 </body>
 </html>
